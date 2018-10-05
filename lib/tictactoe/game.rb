816ae17f9bc821fe6db9e2f3b61b66faa1f12e52
Mithesh <mithesh109@gmail.com>
2018-10-05 12:24:48 +0530

    Adding modules and functions

---
+++ lib/tictactoe/game.rb
@@ -0,0 +1,63 @@
+module TicTacToe
+  module Game
+    extend self
+    
+    def play
+      board = [[nil, nil, nil],
+               [nil, nil, nil],
+               [nil, nil, nil]]
+               
+      players = [:X, :O].cycle
+
+      left_diagonal = [[0,0], [1,1], [2,2]]
+      right_diagonal = [[0,2], [1,1], [2,0]]
+
+      current_player = players.next
+
+      loop do
+        puts board.map {|row| row.map { |e| e || " "}.join("|")}.join("\n")
+        print "\n>> "
+        row, col = gets.split.map { |e| e.to_i }
+        puts
+        begin
+          cell_contents = board.fetch(row).fetch(col)
+        rescue IndexError
+          puts "Out of bounds, try again"
+          next
+        end
+        
+        if cell_contents
+          puts "Cell occupied, please try another position"
+          next
+        end
+        
+        board[row][col] = current_player
+        
+        lines = []
+        
+        [left_diagonal, right_diagonal].each do |line|
+          lines << line if line.include?([row, col])
+        end
+           
+        lines << (0..2).map { |c1| [row, c1] }
+        lines << (0..2).map { |r1| [r1, col] }
+        
+        win = lines.any? do |line|
+          line.all? { |row, col| board[row][col] == current_player }
+        end
+        
+        if win
+          puts "#{current_player} wins!"
+          exit
+        end
+        
+        if board.flatten.compact.length == 9
+          puts "It's a draw"
+          exit
+        end
+        
+        current_player = players.next
+      end
+    end
+  end
+end         
